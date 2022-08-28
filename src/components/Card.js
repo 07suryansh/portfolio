0a70f2694a2044248d44c8e17f48cf27d3243296
Suryansh Singh Bisen <07suryansh@gmail.com>
2022-08-29 01:38:03 +0530

    added transition to card links

--- src/components/Card.js
+++ src/components/Card.js
@@ -31,28 +31,29 @@ function Card() {
           </p>
           <div className="flex align-center justify-center mt-4">
             <a
-              className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300"
+              className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition ease-in-out delay-900 hover:-translate-y-1 hover:scale-110 duration-300"
+              
               href="https://github.com/07suryansh"
             >
               <FaGithub />
               <span className="sr-only">Github</span>
             </a>
             <a
-              className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-blue-600 hover:bg-blue-600 rounded-full hover:text-white transition-colors duration-300"
+              className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-blue-600 hover:bg-blue-600 rounded-full hover:text-white transition ease-in-out delay-900 hover:-translate-y-1 hover:scale-110 duration-300"
               href="https://www.linkedin.com/in/suryansh-singh-bisen-297ba3138/"
             >
               <FaLinkedin />
               <span className="sr-only">LinkedIn</span>
             </a>
             <a
-              className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-red-500 hover:bg-red-500 rounded-full hover:text-white transition-colors duration-300"
+              className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-red-500 hover:bg-red-500 rounded-full hover:text-white transition ease-in-out delay-900 hover:-translate-y-1 hover:scale-110 duration-300"
               href="https://https://www.instagram.com/07suryansh/"
             >
               <FaInstagram />
               <span className="sr-only">Twitter</span>
             </a>
             <a
-              className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-teal-700 hover:bg-black rounded-full hover:text-white transition-colors duration-300"
+              className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-teal-700 hover:bg-black rounded-full hover:text-white transition ease-in-out delay-900 hover:-translate-y-1 hover:scale-110 duration-300"
               href="mailto:07suryansh@gmail.com"
             >
               <FaRegEnvelope />
